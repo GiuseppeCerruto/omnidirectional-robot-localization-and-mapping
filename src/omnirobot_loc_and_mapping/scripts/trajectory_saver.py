7463e70f8a978555b72b7ba79b751b1b86337f8d
Davide Giacomini <giacomini.davide@outlook.com>
2022-06-24 15:07:38 +0200

    Prepared the algorithm for drawing the trajectory. Doesn't work yet

--- src/omnirobot_loc_and_mapping/scripts/trajectory_saver.py
+++ src/omnirobot_loc_and_mapping/scripts/trajectory_saver.py
@@ -2,8 +2,9 @@
 
 import rospy
 from geometry_msgs.msg import PoseWithCovarianceStamped
-from omnirobot_loc_and_mapping.msg import AmclPoses
 from omnirobot_loc_and_mapping.srv import TrajectoryService
+import cv2 as cv
+import os
 
 
 class AmclPosesSaver:
@@ -23,9 +24,29 @@ class AmclPosesSaver:
         self.poses.append(data)
         # self.pub.publish(self.poses)
 
-    def service_callback(self, req):
-        # print(self.poses[-1].header.seq)
-        return self.poses[-1].header.seq
+    def service_callback(self, req):    
+        script_directory_path = os.path.dirname(__file__)
+        map_path = os.path.join(script_directory_path, '../maps/map2.pgm')
+        dest_path = os.path.join(script_directory_path, '../maps/map_with_trajectory.jpg')
+
+        img = cv.imread(map_path, cv.WINDOW_NORMAL);
+        # imS = cv.resize(img, (800, 800))  
+        # cv.imshow("Output", imS);
+        # cv.waitKey(0)
+
+        for i in range(len(self.poses)-1):
+            curr_x = self.poses[i].pose.pose.position.x
+            curr_y = self.poses[i].pose.pose.position.y
+            next_x = self.poses[i+1].pose.pose.position.x
+            next_y = self.poses[i+1].pose.pose.position.y
+            cv.line(img,(curr_x, curr_y),(next_x, next_y), (255,0,0), 5)
+
+            rospy.loginfo("NUMBER OF POSES: " + str(len(self.poses)) + " ------>")
+            rospy.loginfo("\ncurr_x " + str(curr_x) + "\ncurr_y " + str(curr_y) + "\nnext_x " + str(next_x) + "\nnext_y " + str(next_y) + "\n")
+
+        cv.imwrite(dest_path, img)
+
+        return True
     
 
 if __name__ == '__main__':
